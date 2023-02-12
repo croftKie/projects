f775079eea2ce3f45e852605e3ae6b96dd5d6096
Kieran Croft <kiecroft@gmail.com>
2023-02-12 14:33:52 +0000

    added experience spans and links to outside references

--- src/js/index.js
+++ src/js/index.js
@@ -18,3 +18,17 @@ panels.forEach((panel)=>{
   panel.addEventListener('mouseenter',mouseEnterSaturate);
   panel.addEventListener('mouseexit',mouseEnterSaturate);
 });
+
+let experienceText = Array.from(document.querySelectorAll('span'));
+let listItems = Array.from(document.querySelectorAll('li'));
+
+listItems.forEach((item)=>{
+  item.addEventListener('mouseenter',()=>{
+    experienceText[listItems.indexOf(item)].classList.add('open');
+  });
+  item.addEventListener('mouseleave',()=>{
+    experienceText[listItems.indexOf(item)].classList.remove('open');
+  });
+});
+
+console.log(experienceText, listItems);
